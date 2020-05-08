01c67c6d16d9fed3bd6c031e3955471ddba1e60a
LDR8 <54103200+LDR8@users.noreply.github.com>
2020-05-08 11:49:47 +0200

    Added option for batch processing and new function
    New argument -B to shift the brightness of black pixels from the source image to simulate backlight shining.

--- RGB-Matrix.py
+++ RGB-Matrix.py
@@ -1,32 +1,41 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageChops
 import sys, os, datetime, argparse
 
+
+# Standard values
 export_fformat = '.jpg' # Output-Format
 export_fformats = {'.jpg': 'JPEG', '.png': 'PNG'}
 
-ext = '_rgb-matrix' #filename extension for the output file
+ext = '_rgb-matrix' # filename extension for the output file
 
-pixel_width = 2 #width of each pixel - so three units (rgb) of this value must go into one block
-pixel_height = 5 #height of the pixels (value must go into one block)
+pixel_width = 2 # width of each pixel - so three units (rgb) of this value must go into one block
+pixel_height = 5 # height of the pixels (value must go into one block)
+block = 7 # block width - 1 block contains one red, one greed and one blue pixel - black space inclusive
 
-block = 7 #block width - 1 block contains one red, one greed and one blue pixel - black space inclusive
+black = 0 # shift black to simulate display backlight
 
 valid_formats = ['.bmp', '.jpeg', '.jpg', '.png', '.tiff', '.tif', '.webp']
+
+output_path = ''
+fn = ''
+fext = ''
+new_fn = ''
 
 
 # argparse
 parser = argparse.ArgumentParser(description='Render a display RGB matrix from a source image.')
-parser.add_argument('file', type=str, metavar='', help='Filename of Source-Image')
+parser.add_argument('file', type=str, metavar='', help='Filename of a single Source-Image, or a directory for batch processing.')
 parser.add_argument('-f', '--format', type=str, metavar='', help='Export File-Format (.jpg or .png)')
 parser.add_argument('-w', '--width', type=int, metavar='', help='Width of each individual Pixel (R-G-B).')
 parser.add_argument('-H', '--height', type=int, metavar='', help='Height of each individual Pixel (R-G-B).')
 parser.add_argument('-b', '--block', type=int, metavar='', help='Block-Size defines the overall size of the repeated Pixel-Pattern')
+parser.add_argument('-B', '--black', type=int, metavar='', help='Shift brightness of black pixels to simulate backlight shining - otherwise no pixels are visible in black areas. Value between 0-100 (0 = black 100 = white)')
 args = parser.parse_args()
 
-source = args.file #Source image filename
-new_fn = ''
+source = args.file # Source image filename
 
-#error correction for bad input values
+
+# error correction for bad input values
 if isinstance(args.width, int) and isinstance(args.height, int) and isinstance(args.block, int):
 	pixel_width = args.width
 	pixel_height = args.height
@@ -46,12 +55,22 @@ if isinstance(args.width, int) and isinstance(args.height, int) and isinstance(a
 		pixel_width = int(block/3)
 		pixel_height = block
 
-# Test if given output file format is supported
+if isinstance(args.black, int):
+	if args.black in range(0, 101):
+		black = args.black
+	else:
+		print("Value for -B --black must be between 0 and 100.")
+		exit()
+
+
+# Test if desired output file format is supported
 if isinstance(args.format, str):
 	if args.format in export_fformats:
 		export_fformat = args.format
 	else:
 		print("Unsupported Fileformat.")
+		exit()
+
 
 def main(source):
 
@@ -71,9 +90,18 @@ def main(source):
 	i1 = Image.new('RGB', (x_new,y_new))
 	d1 = ImageDraw.Draw(i1)
 
-	process(img, d1, x_orig, y_orig)
+	if black != 0:
+		img = chop(img, img.size)
 
-	i1.save(new_fn + export_fformat, export_fformats[export_fformat])
+	process(img, d1, x_orig, y_orig)
+	
+	#t = datetime.datetime.now()
+	#new_fn = fn + ext + '_' + t.strftime("%d-%m-%Y_%H-%M-%S")
+	new_fn = fn + ext
+	print(f'Image saved as {new_fn}{export_fformat}')
+
+	i1.save(output_path + new_fn + export_fformat, export_fformats[export_fformat])
+
 
 def process(img, d1, x_orig, y_orig):
 
@@ -122,27 +150,79 @@ def process(img, d1, x_orig, y_orig):
 	
 			x_count = 0
 			y_count+=block
+
+
+# superimpose source image with grey image
+def chop(image, size):
+
+	bg_color = int((255 / 100) * black)
+
+	#Create new Image
+	image_new = Image.new('RGB', size, (bg_color, bg_color, bg_color))
+	result = ImageChops.screen(image, image_new)
+	return result
 			
 
 if __name__ == "__main__":
 
-	#Check if source file exists
+	# Check if source image exists
 	if os.path.isfile(source):
 		fn, fext = os.path.splitext(source)
-		
-		t = datetime.datetime.now()
-		new_fn = fn + ext + '_' + t.strftime("%d-%m-%Y_%H-%M-%S")
 
-		#Check if source file format is supported
+		# Check if source file format is supported
 		if (fext in valid_formats) or (str.lower(fext) in valid_formats):
 			
 			print(f'Found image {source}')
 			print('Starting process...')
 			main(source)
-			print(f'Result saved as {new_fn}{export_fformat}')
 			
 		else:
 			print(f'{fext} file is not supported. \nSupported formats are: {valid_formats}')
+	
+	# Folder as source
+	elif os.path.isdir(source) or os.path.isdir('./' + source):
+		counter = 1
+		fn_list = []
+		fext_list = []
+		for file in os.listdir(source):
+			fn, fext = os.path.splitext(file)
+			
+			# Check if source file format is supported
+			if (fext in valid_formats) or (str.lower(fext) in valid_formats):
+				fn_list.append(fn)
+				fext_list.append(fext)
+		
+		if len(fn_list)>=1:
+			status = True
+			yes = ['yes', 'y', '']
+			no = ['no', 'n']
+			
+			# Ask to start batch process
+			while status:
+				choice = input(f'{len(fn_list)} images fount - start process now? (y/n)').lower()
+				
+				if choice in yes:
+					print('Batch process started...')
+					
+					for file in range(0, len(fn_list)):
+						fn = fn_list[file]
+						fext = fext_list[file]
+						output_path = source + '/'
+						main(output_path+fn+fext)
+					
+					print('Batch process finished!')
+						
+					status = False
+					
+				elif choice in no:
+					print('exiting')
+					status = False
+					
+				else:
+					print('Please answer with yes/no (y/n).')
+					
+		else:
+			print(f'No supported images found. \nSupported formats are: {valid_formats}')
 
 	else:
-		print(f'Couldn\'t find the image {source}')
+		print(f'File not found: {source}')
